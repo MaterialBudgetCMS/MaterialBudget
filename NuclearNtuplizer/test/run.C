b94da4e47531ff506793819501477e78cef71dd7
jungmin <jungmin@cern.ch>
2015-06-01 13:18:51 +0200

    code for efficiency calculation

---
+++ NuclearNtuplizer/test/run.C
@@ -0,0 +1,4 @@
+{
+ gROOT->ProcessLine(".L NtupleReaderNuclearInteractions_2015.C++");
+ gROOT->ProcessLine(".x runNtupleReaderNuclearInteractions_2015.cc");
+ }
--- NuclearNtuplizer/test/runNtupleReaderNuclearInteractions_2015.cc
+++ NuclearNtuplizer/test/runNtupleReaderNuclearInteractions_2015.cc
@@ -1,6 +1,6 @@
 void runNtupleReaderNuclearInteractions_2015( const std::string fileList = "prova.txt", const Long64_t maxEvents = -1 )
 {
-  gSystem->CompileMacro("NtupleReaderNuclearInteractions_2015.C");
+  //gSystem->CompileMacro("NtupleReaderNuclearInteractions_2015.C");
   NtupleReaderNuclearInteractions_2015 m( fileList, maxEvents );
   m.beginJob();
   m.analyze();
