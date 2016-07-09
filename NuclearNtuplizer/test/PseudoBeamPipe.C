2633c2a46e31aa8ad852fac31ac776798445d6de
mgouzevi <mgouzevi@cern.ch>
2016-07-09 09:32:54 +0200

    UPdated

--- NuclearNtuplizer/test/PseudoBeamPipe.C
+++ NuclearNtuplizer/test/PseudoBeamPipe.C
@@ -1,12 +1,12 @@
 
 // all units are in cm
-void PseudoBeamPipe(double R0, double x0, double y0, double width){
+void PseudoBeamPipe(double R0, double x0, double y0, double width, int Nmax){
 
   TRandom3* rnd = new TRandom3(1000);
 
   TH2D* GenCircle = new TH2D("GenCircle", " Generated Circle; x (cm); y (cm)", 100, -5, 5, 100, -5, 5);
   TH2D* GenCircleRPhi = new TH2D("GenCircleRPhi", " Generated Circle; phi (cm); y (cm)", 100, 0, 2.*TMath::Pi(), 100, 0, 5);
-  for (int i = 0; i < 1e6; i++){
+  for (int i = 0; i < Nmax; i++){
 
     if (i%200000 == 0) cout << i << endl;
     double phi = rnd->Uniform()*2*TMath::Pi();
@@ -31,7 +31,7 @@ void PseudoBeamPipe(double R0, double x0, double y0, double width){
   GenCircleRPhi->SetStats(0);
   GenCircleRPhi->Draw("COLZ");
 
-  TFile* file_out = new TFile(Form("PseudoBeamPipe_r%.2fcm_x0%.2fcm_y0%.2fcm_w%.2fcm.root", R0, x0, y0, width), "RECREATE");
+  TFile* file_out = new TFile(Form("PseudoBeamPipe_r%.2fcm_x0%.2fcm_y0%.2fcm_w%.2fcm_Nevt%d.root", R0, x0, y0, width, Nmax), "RECREATE");
   GenCircle->Write();
   GenCircleRPhi->Write();
   file_out->Close();
