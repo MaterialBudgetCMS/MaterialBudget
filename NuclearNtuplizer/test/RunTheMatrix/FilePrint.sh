866d4a94c6658d04831078fb89a1d40fa1f551b7
Anna <kropiv@cern.ch>
2018-06-18 13:23:34 +0200

    Print all files from directory for python configuration

---
+++ NuclearNtuplizer/test/RunTheMatrix/FilePrint.sh
@@ -0,0 +1,10 @@
+DirName=$1
+#DirName="/eos/cms/store/group/dpg_tracker_strip/tracker/MaterialBudget/NI/PionGun2018/CMSSW_10_1_6_Pion10GeV_step1/"
+
+
+rm -rf FileList_Mod.txt
+ls -all $DirName|grep root > FileList.txt
+echo  $DirName
+#awk -v z="$DirName" 'BEGIN {print "'\''file:"z$NF"'\'',"}' FileList.txt > FileList_Mod.txt 
+awk -v z="$DirName" '{print "'\''file:"z$NF"'\'',"}' FileList.txt > FileList_Mod.txt 
+rm -rf FileList.txt
