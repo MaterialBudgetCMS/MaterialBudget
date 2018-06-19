99639413baee9c87e35e7aa0a33f1e1b5ca73bd7
Anna <kropiv@cern.ch>
2018-06-19 14:00:02 +0200

    udate print root files from directory

--- NuclearNtuplizer/test/RunTheMatrix/FilePrint.sh
+++ NuclearNtuplizer/test/RunTheMatrix/FilePrint.sh
@@ -1,5 +1,7 @@
 DirName=$1
 #DirName="/eos/cms/store/group/dpg_tracker_strip/tracker/MaterialBudget/NI/PionGun2018/CMSSW_10_1_6_Pion10GeV_step1/"
+#DirName="/eos/cms/store/group/dpg_tracker_strip/tracker/MaterialBudget/NI/PionGun2018/CMSSW_10_1_6_Pion10GeV_step2/"
+#DirName="/eos/cms/store/group/dpg_tracker_strip/tracker/MaterialBudget/NI/PionGun2018/CMSSW_10_1_6_Pion10GeV_RECO/"
 
 
 rm -rf FileList_Mod.txt
