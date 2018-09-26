69b2c8da3c175560a0fb2a2f5a88f9db22ef6868
Anna <kropiv@cern.ch>
2018-09-26 15:47:42 +0200

    2018 reco data for crab

---
+++ NuclearNtuplizer/test/crab/samples/sample_Run2018D_21Aug.py
@@ -0,0 +1,13 @@
+
+########
+# Run2015D: 25 ns DATA:
+########
+ 
+samples['as_Run2018_PromptReco_run315646']        = ['/EGamma/Run2018D-21Aug2018-v1/RECO',  ['globalTag=102X_dataRun2_Prompt_EgammaRepro_v2']] #CMSSW_10_2_1
+
+
+########
+# Alternative global configuration
+########
+# config.Data.outLFNDirBase = '/store/user/piedra/test'
+# config.Site.storageSite   = 'T2_ES_IFCA'
