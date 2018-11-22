5be6d683b8bc5390552bd77aa32fd3deef8a850a
Anna <kropiv@cern.ch>
2018-11-22 16:21:41 +0100

    add to process RAW to RECO for 2018C

---
+++ NuclearNtuplizer/test/crab/samples/sample_Run2018C_SingleMu_RawToReco.py
@@ -0,0 +1,13 @@
+
+########
+# Run2015D: 25 ns DATA:
+########
+ 
+samples['SingleMu_Run2018C_RawToReco']        = ['/SingleMuon/Run2018C-v1/RAW',  ['globalTag=102X_dataRun2_Prompt_v7']] #CMSSW_10_2_1
+
+
+########
+# Alternative global configuration
+########
+# config.Data.outLFNDirBase = '/store/user/piedra/test'
+# config.Site.storageSite   = 'T2_ES_IFCA'
